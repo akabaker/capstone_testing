1f82febc7e4439433bf81849752d410619f18d38
Luke <akabaker@gmail.com>
2011-04-06 23:46:47 -0500

    Added polyline function

--- static/js/map.js
+++ static/js/map.js
@@ -1,10 +1,26 @@
 function initialize() {
-	var latlng = new google.maps.LatLng(-34.397, 150.644);
+	var latlng = new google.maps.LatLng(38.94617, -92.32866);
 	var myOptions = {
-		zoom: 8,
+		zoom: 19,
 		center: latlng,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	var map = new google.maps.Map(document.getElementById("map-canvas"),
-		myOptions);
-};
+
+	var map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
+
+	var polyOptions = {
+		strokeColor: '#000000',
+		strokeOpacity: 1.0,
+		strokeWeight: 3
+	};
+
+	poly = new google.maps.Polyline(polyOptions);
+	poly.setMap(map);
+
+	google.maps.event.addListener(map, 'click', addLatLng);
+}
+
+function addLatLng(event) {
+	var path = poly.getPath();
+	path.push(event.latLng);
+}
