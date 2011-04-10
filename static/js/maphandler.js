e05736d8bc791b5040186ba9fcd529b5859b87d4
Luke <akabaker@gmail.com>
2011-04-10 14:30:00 -0500

    branching

--- static/js/maphandler.js
+++ static/js/maphandler.js
@@ -1,50 +1,55 @@
-var MapHandler = function() {
+function MapHandler() {
+	this.geoCodeBtn = document.getElementById('mh-go');
+	this.address = document.getElementById('mh-address');
+}
 
-	geoCodeBtn: document.getElementById('mh-go'),
-	address: document.getElementById('mh-address'),
+MapHandler.prototype.getGeoCodeBtn = function() {
+	return this.geoCodeBtn;
+}
 
-	addLatLng: function(event) {
-		console.log(this.getAddress);
-		var path = poly.getPath();
-		path.push(event.latLng);
-	},
+MapHandler.prototype.getAddress = function() {
+	return this.address;
+}
 
-	geoCode: function() {
-		//console.log(MapHandler.address);
-	},
+MapHandler.prototype.addLatLng = function(event) {
+	var path = poly.getPath();
+	path.push(event.latLng);
+}
 
-	addListener: function(el, ev, fn) {
-		if (el.addEventListener) {
-			el.addEventListener(ev, fn, false);
-		} else if (el.attachEvent) {
-			el.attachEvent('on' + ev, fn);
-		} else {
-			el['on' + ev] = fn;
-		}
-	},
+MapHandler.prototype.geoCode = function() {
+	alert(this.address);
+}
 
-	initialize: function() {
-	
-		this.addListener(this.geoCodeBtn, 'click', this.geoCode);
-
-		var latlng = new google.maps.LatLng(38.94617, -92.32866);
-		var myOptions = {
-			zoom: 19,
-			center: latlng,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-
-		var map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
-
-		var polyOptions = {
-			strokeColor: '#000000',
-			strokeOpacity: 1.0,
-			strokeWeight: 3
-		};
-
-		poly = new google.maps.Polyline(polyOptions);
-		poly.setMap(map);
-
-		google.maps.event.addListener(map, 'click', addLatLng);
+MapHandler.prototype.addListener = function(el, ev, fn) {
+	if (el.addEventListener) {
+		el.addEventListener(ev, fn, false);
+	} else if (el.attachEvent) {
+		el.attachEvent('on' + ev, fn);
+	} else {
+		el['on' + ev] = fn;
 	}
-};
+}
+
+MapHandler.prototype.initialize = function() {
+	this.addListener(this.geoCodeBtn, 'click', this.geoCode);
+
+	var latlng = new google.maps.LatLng(38.94617, -92.32866);
+	var myOptions = {
+		zoom: 19,
+		center: latlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	var map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
+
+	var polyOptions = {
+		strokeColor: '#000000',
+		strokeOpacity: 1.0,
+		strokeWeight: 3
+	};
+
+	poly = new google.maps.Polyline(polyOptions);
+	poly.setMap(map);
+
+	google.maps.event.addListener(map, 'click', this.addLatLng);
+}
